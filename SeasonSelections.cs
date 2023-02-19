422efed11ca2089a6750c870c05c678cc460a228
Sky-Meteor <2746501855@qq.com>
2023-02-19 12:17:13 +0800

    SeasonSelections localization key

--- SeasonSelections.cs
+++ SeasonSelections.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria.ModLoader.Config;
 
 namespace Fargowiltas
 {
     public enum SeasonSelections
     {
+        [Label("$Mods.Fargowiltas.Config.SeasonSelectionNormal")]
         Normal,
+        [Label("$Mods.Fargowiltas.Config.SeasonSelectionAlwaysOn")]
         AlwaysOn,
+        [Label("$Mods.Fargowiltas.Config.SeasonSelectionAlwaysOff")]
         AlwaysOff
     }
 }
